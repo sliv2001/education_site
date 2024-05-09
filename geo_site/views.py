875915e73e8d595ca40f00ec58c0f24568b2e188
Ivan Sladkov <ivan.sladkov@yandex.ru>
2024-05-10 01:18:17 +0300

    [dev/0] Added login button

--- geo_site/views.py
+++ geo_site/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Country
+from .models import Country, LearnRate
 
 def index(request):
-    total_countries = Country.objects.count()
     context = {
-        'total_countries': total_countries,
+        'total_countries': Country.objects.count(),
         'user_is_auth': request.user.is_authenticated,
-        'username': request.user.username
+        'username': request.user.username,
+        'learned_countries': LearnRate.objects.filter(repetition_number__gt=0, user=request.user).count() if request.user.is_authenticated else 0,
     }
     return render(request, "index.html", context=context)
