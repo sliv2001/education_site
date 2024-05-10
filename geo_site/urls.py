3a51f0b8ef54a6d2cbdc4f739a6cc7dda43c6ffb
Ivan Sladkov <ivan.sladkov@yandex.ru>
2024-05-10 19:04:35 +0300

    [dev/0] Added learning match

--- geo_site/urls.py
+++ geo_site/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("learn/match", views.learn_match, name="learn"),
+    path("hoorah", views.hoorah, name="learn"),
     path("learn/5", views.learn_end, name="learn"),
     path("learn/", views.learn_start, name="learn"),
     path("learn/<int:number>", views.learn, name="learn")
--- geo_site/views.py
+++ geo_site/views.py
@@ -3,6 +3,7 @@ import random
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Country, LearnRate
 
 def getCountriesNum():
@@ -15,19 +16,18 @@ def index(request):
         'username': request.user.username,
         'learned_countries': LearnRate.objects.filter(repetition_number__gt=0, user=request.user).count() if request.user.is_authenticated else 0,
     }
-    # if request.method == 'POST':
 
     return render(request, "index.html", context=context)
 
 def learn(request, number):
     context = {
+        'user_is_auth': request.user.is_authenticated,
+        'username': request.user.username,
         'country': request.session['countries'][number][0],
         'capital': request.session['countries'][number][1],
         'region': request.session['countries'][number][2],
         'current_number': number,
         'next_number': number+1,
-        'user_is_auth': request.user.is_authenticated,
-        'username': request.user.username,
     }
     country_entry = Country.objects.get(name=request.session['countries'][number][0])
     try:
@@ -55,7 +55,40 @@ def getCountries(number):
 def learn_start(request):
     countries = getCountries(5)
     request.session['countries'] = countries
+    request.session['wrong_answer']=False
     return redirect(learn, number=0)
 
 def learn_end(request):
+    request.session['wrong_answer']=False
     return redirect(index)
+
+def learn_match(request):
+    countries_capitals = request.session['countries']
+    if (request.method=='POST'):
+        for country, capital, region in countries_capitals:
+            if request.POST.get(country, "") != capital:
+                request.session['wrong_answer']=True
+                return redirect(learn_match)
+        messages.success(request, "Учебный тест пройден успешно!")
+        request.session['practice_finished']=True
+        return redirect(hoorah)
+    
+    # If GET request, we shuffle countries and ask user to answer
+    countries = ([countries_capitals[i][0] for i in range(len(countries_capitals))])
+    capitals = ([countries_capitals[i][1] for i in range(len(countries_capitals))])
+    random.shuffle(capitals)
+    countries_capitals = [[countries[i], capitals[i]] for i in range(len(capitals))]
+    context={
+        'user_is_auth': request.user.is_authenticated,
+        'username': request.user.username,
+        'countries': countries_capitals,
+        'wrong_answer': request.session['wrong_answer']
+    }
+    return render(request, "learn_match.html", context=context)
+
+def hoorah(request):
+    context={
+        'user_is_auth': request.user.is_authenticated,
+        'username': request.user.username,
+    }
+    return render(request, "hoorah.html", context=context)
